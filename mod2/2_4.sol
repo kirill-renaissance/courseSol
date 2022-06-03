3e7d9e0ccc3fdf032bff2b8125bfd4d3e5d56ef7
ki <60847687+kirill-renaissance@users.noreply.github.com>
2022-06-03 18:46:13 +0300

    Create 2_4.sol

---
+++ mod2/2_4.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+contract Bonus {
+    mapping (address => uint256[2]) _balances;
+    uint256 public x;
+    function calculateBalances(uint256 accrue, uint256 deduct) public virtual returns(uint userBalance)
+    {
+        require(_balances[msg.sender][0] + accrue >= _balances[msg.sender][1] + deduct,"erroRRRR");
+        _balances[msg.sender][0] += accrue;
+        _balances[msg.sender][1] += deduct;
+        userBalance = _balances[msg.sender][0] - _balances[msg.sender][1];
+        x = userBalance;
+        return  userBalance;
+    }
+}
+
+contract Vip is Bonus {
+function calculateBalances(uint256 accrue, uint256 deduct) public override(Bonus) returns(uint userBalance)
+    {
+        require(_balances[msg.sender][0] + 2 * accrue >= _balances[msg.sender][1] + deduct,"erroRRRR");
+        _balances[msg.sender][0] += 2 * accrue;
+        _balances[msg.sender][1] += deduct;
+        userBalance = _balances[msg.sender][0] - _balances[msg.sender][1];
+        x = userBalance;
+        return  userBalance;
+    }
+}
+
+contract Premium is Bonus {
+function calculateBalances(uint256 accrue, uint256 deduct) public override(Bonus) returns(uint userBalance)
+    {
+        require(_balances[msg.sender][0] + 5 * accrue >= _balances[msg.sender][1] + deduct,"erroRRRR");
+        _balances[msg.sender][0] += 5 * accrue;
+        _balances[msg.sender][1] += deduct;
+        userBalance = _balances[msg.sender][0] - _balances[msg.sender][1];
+        x = userBalance;
+        return  userBalance;
+    }
+}
